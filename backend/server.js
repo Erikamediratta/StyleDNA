18eb40818896accc2129b120ef7f53c73d294251
Erika Mediratta <erikamediratta@gmail.com>
2026-07-01 15:37:47 +0530

    Updated cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5000", credentials: true }));
+app.use(cors({ origin:[ "http://localhost:5000",  "https://grwm-agent.onrender.com"], credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/wardrobe", wardrobeRoutes); 
